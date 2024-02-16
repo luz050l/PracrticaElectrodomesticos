c6ec9affcfdd62dc039fc86b4ca454352c2fd645
luz050l <149832493+luz050l@users.noreply.github.com>
2024-02-15 19:01:30 -0600

    Add files via upload

---
+++ electrodomesticos.py
@@ -0,0 +1,43 @@
+a="a"
+b="b"
+c="c"
+d="d"
+e="e"
+f="f"
+
+class Electrodomestico:
+    def __init__(self,PrecioB,Color,Peso,ConsumoE):
+        self.PrecioB= PrecioB
+        self.Color= Color           #como le hago cuando tengo mas de 1 caractes (?????????????????) x2
+        self.ConsumoE= ConsumoE
+        self.Peso= Peso 
+        self.letra=a,b,c,d,e,f  #como le hago cuando tengo mas de 1 caractes (?????????????????) hacer lo mismo que en el sexo, debo comparar letra por letra (?)
+# def mujer(self):
+        #if self.sexo==False:
+         #   print("La persona es hombre") 
+        #else:
+         #   self.mujer=True
+    #def hombre(self):
+     #   if self.sexo:
+      #      self.sexo=False
+       # else:
+        #    print("Es mujer")
+
+    def letras(a,b,c,d,e,f):
+        if self.letra==a:
+            print("Letra Correcta")
+        elif self.letra==b:
+            print("Letra Correcta")
+        elif self.letra==c:
+            print("Letra Correcta")
+        elif self.letra==d:
+            print("Letra Correcta")
+        elif self.letra==e:
+            print("Letra Correcta")
+        elif self.letra==f:
+            print("Letra Correcta")
+        else:
+            print("Incorrecta"+ letra)
+            print("Por default se pondra A, Gracias")
+    def colors():
+        
